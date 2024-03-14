3bc1254f0cab967b7cde991195e3983d9935278f
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-03-14 23:01:09 +0000

    Update matrixEQTL_SaveResults.sh

--- matrixEQTL_SaveResults.sh
+++ matrixEQTL_SaveResults.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #!/bin/bash
-#SBATCH -A Research_Project-MRC164847 # research project to submit under.
+#SBATCH -A Research_Project1 # research project to submit under.
 #SBATCH --export=ALL # export all environment variables to the batch job.
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq
@@ -20,18 +20,20 @@ set -e
 ### NOTE: Do not store confidenial information in this file use the config file
 
 ######
-InDir=./Inputs
+InDir=./Results/UKBBN
 FilePrefix=UKBBN
-Dist=1e+6
-cis_pval=1
-trans_pval=1
+Dist=250000
+cis_pval=0.05
+trans_pval=0.05
 trans_cross_chr=no
+save_csv_cis=yes
+save_csv_trans=yes
 
 SCRIPTDIR=./R
 
 module load R
 
-Rscript ${SCRIPTDIR}/matrixQTL_eQTL_SaveResults.R $InDir $FilePrefix $cis_pval $trans_pval $Dist $trans_cross_chr
+Rscript ${SCRIPTDIR}/matrixQTL_eQTL_SaveResults.R $InDir $FilePrefix $cis_pval $trans_pval $Dist $trans_cross_chr $save_csv_cis $save_csv_trans
 
 echo Job finished:
 date -u
