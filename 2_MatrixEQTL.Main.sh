87dd7455347c0601e31b8d164374ae6c3be36838
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-06-14 00:09:24 +0100

    Add files via upload

---
+++ 2_MatrixEQTL.Main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -A Research_Project-MRC164847 # research project to submit under.
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -D . # set working directory to .
+#SBATCH -p mrcq
+#SBATCH --time=00:05:00 # Maximum wall time for the job
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --ntasks-per-node=16 # specify number of processors.
+#SBATCH --mail-type=END # send email at job completion
+#SBATCH --mail-user=m.kouhsar@exeter.ac.uk # email address
+
+source $1
+
+OutDir=$(dirname "$OutPrefix")
+mkdir -p $OutDir
+
+if [ $chr == all ]
+then
+  array=($(seq 1 1 22))  ## seq FIRST STEP LAST
+  printf -v chr '%s,' "${array[@]}"
+  chr="${chr%,}"
+fi
+
+
+sbatch --array=$chr ${ScriptDir}/matrixQTL_eQTL_main.sh $OutPrefix $Dist $CisPvalue $TransPvalue $TransCrossChr
+
+
+
