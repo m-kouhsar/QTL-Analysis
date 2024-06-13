c206a3baf145cdb91901138fa519e30cb59e450b
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-06-14 00:11:57 +0100

    Update 3_MatrixEQTL.SaveResults.sh

--- 3_MatrixEQTL.SaveResults.sh
+++ 3_MatrixEQTL.SaveResults.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #!/bin/bash
-#SBATCH -A Research_Project-MRC164847 # research project to submit under.
+#SBATCH -A Research_Project1 # research project to submit under.
 #SBATCH --export=ALL # export all environment variables to the batch job.
 #SBATCH -D . # set working directory to .
 #SBATCH -p mrcq
-#SBATCH --time=5:00:00 # Maximum wall time for the job
+#SBATCH --time=1:00:00 # Maximum wall time for the job
 #SBATCH --nodes=1 # specify number of nodes.
 #SBATCH --ntasks-per-node=16 # specify number of processors.
 #SBATCH --mail-type=END # send email at job completion
