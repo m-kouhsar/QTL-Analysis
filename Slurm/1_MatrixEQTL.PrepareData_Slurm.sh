d5318e2743aaf7eff0b571eeacfc53100b45d829
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-12-04 12:15:42 +0000

    Update 1_MatrixEQTL.PrepareData_Slurm.sh

--- Slurm/1_MatrixEQTL.PrepareData_Slurm.sh
+++ Slurm/1_MatrixEQTL.PrepareData_Slurm.sh
@@ -11,6 +11,14 @@
 
 source $1
 
+Overwrite="${Overwrite#[[:space:]]}"
+Overwrite="${Overwrite%[[:space:]]}"
+Overwrite="$(echo "$Overwrite" | tr '[:upper:]' '[:lower:]')"
+
+chr="${chr#[[:space:]]}"
+chr="${chr%[[:space:]]}"
+chr="$(echo "$chr" | tr '[:upper:]' '[:lower:]')"
+
 OutDir=$(dirname "$OutPrefix")
 mkdir -p $OutDir
 
@@ -23,20 +31,21 @@ fi
 
 for i in "${array[@]}"
 do
-  if [ ! -f ${OutPrefix}.chr${i}.raw ]
+  if [ ! -f ${OutPrefix}.chr${i}.raw ] || [ $Overwrite = "yes" ]
   then
-    echo "Running chr ${i}..."
+    echo "Converting binary format chromosome $i..."
     plink --bfile ${GenotypeBinaryPrefix} --recodeA --chr $i --out ${OutPrefix}.chr${i}
     echo "#########################################################################################"
   fi
 done
 
-if [ ! -f ${OutPrefix}.eigenvec ]
+if [ ! -f ${OutPrefix}.eigenvec ] || [ $Overwrite = "yes" ]
 then
+  echo "Calculating genotype data PCs..."
   gcta64 --bfile ${GenotypeBinaryPrefix} --make-grm-bin --out ${OutPrefix} --thread-num 16
   gcta64 --grm ${OutPrefix} --pca --out ${OutPrefix}
 fi
 
 
-Rscript ${ScriptDir}/PrepareData.R ${GenotypeBinaryPrefix}.fam ${OutPrefix}.eigenvec $ExpressionFile $PhenotypeFile $GeneLocationFile "$FactCovar" "$NumCovar" "$OutPrefix" $chr
+Rscript ${ScriptDir}/PrepareData.R ${GenotypeBinaryPrefix}.fam ${OutPrefix}.eigenvec $ExpressionFile $PhenotypeFile $GeneLocationFile "$FactCovar" "$NumCovar" "$OutPrefix" $chr $Overwrite
 
